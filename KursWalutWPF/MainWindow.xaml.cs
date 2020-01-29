d757496874f0755a7c28ce5f93aa36db3817c201
xYahik <25422544+xYahik@users.noreply.github.com>
2020-01-29 23:49:43 +0100

    KursWalut

---
+++ KursWalutWPF/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using KursWalutLibrary;
+namespace KursWalutWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Licz_Click(object sender, RoutedEventArgs e)
+        {
+            DateTime startDate = DataPoczatkowa.DisplayDate;
+            DateTime endDate = DataKoncowa.DisplayDate;
+            string kodwaluty = KodWaluty.Text;
+            KursWalut nowykurs = new KursWalut();
+            
+            nowykurs.SetDatesList(startDate, endDate);
+            nowykurs.Initialize(kodwaluty);
+
+
+            float srednia = KursWalut.ObliczSrednia(nowykurs._money);
+            SredniKurs.Content = srednia.ToString("n4");
+
+            float odchylenie = KursWalut.LiczOdchylenieStandardowe(nowykurs._money, srednia);
+            Odchylenie.Content = odchylenie.ToString("n4");
+
+            float kursminimalny = KursWalut.KursMinimalny(nowykurs._money);
+            KursMIN.Content = kursminimalny.ToString("n4");
+
+            float kursmaksymalny = KursWalut.KursMaksymalny(nowykurs._money);
+            KursMAX.Content = kursmaksymalny.ToString("n4");
+        }
+    }
+}
--- KursyWalut/Program.cs
+++ KursyWalut/Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Xml.Linq;
+using KursWalutLibrary;
 namespace KursyWalut
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Podaj kod waluty (USD, EUR, CHF, GBP)");
+            string _currencyCode = Console.ReadLine();
+
+            Console.WriteLine("Podaj date poczatkowa [RRRR-MM-DD]");
+            string _startDate = Console.ReadLine();
+
+            Console.WriteLine("Podaj date koncowa [RRRR-MM-DD]");
+            string _endDate = Console.ReadLine();
+
+            DateTime startDate = DateTime.Parse(_startDate);
+            DateTime endDate = DateTime.Parse(_endDate);
+            string moneycode = _currencyCode;
+
+            KursWalut nowykurs = new KursWalut();
+            nowykurs.SetDatesList(startDate, endDate);
+            Console.WriteLine("INITIALIZE...");
+            nowykurs.Initialize(moneycode);
+
+            
+            float srednia = KursWalut.ObliczSrednia(nowykurs._money);
+            Console.WriteLine(srednia.ToString("n4"));
+
+            float odchylenie = KursWalut.LiczOdchylenieStandardowe(nowykurs._money, srednia);
+            Console.WriteLine(odchylenie.ToString("n4"));
+
+            float kursminimalny = KursWalut.KursMinimalny(nowykurs._money);
+            Console.WriteLine(kursminimalny.ToString("n4"));
+
+            float kursmaksymalny = KursWalut.KursMaksymalny(nowykurs._money);
+            Console.WriteLine(kursmaksymalny.ToString("n4"));
+            
+
+
+            Console.WriteLine("FINISHED");
+            Console.ReadKey();
         }
+        
     }
 }
